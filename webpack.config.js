7444ee412ff415389fa531ccee84c9d863bb29f0
IreneGalca <54726113+IreneGalca@users.noreply.github.com>
2022-02-25 19:46:14 -0700

    Add project files.

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+// JavaScript source code
+var path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: path.join(__dirname, './index.jsx'),
+    output: {
+        path: __dirname,
+        filename: 'app.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx/,
+                exclude: /node_modules/,
+                use: 'babel-loader'
+            }
+        ]
+    }
+}
